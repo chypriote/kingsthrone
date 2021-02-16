e531f0ce5923c4a430722f18bf77a3da5ff168d1
NicolasT <nicolastem@hotmail.fr>
2021-02-16 11:05:21 +0100

    init strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8b8181dab43540e7262d42bc374ebe45'),
+    },
+  },
+});

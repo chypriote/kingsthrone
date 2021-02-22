40951d729860dcd3082c661c276d710a24f126c8
NicolasT <nicolastem@hotmail.fr>
2021-02-22 18:08:50 +0100

    return ranks as rankings in player infos

--- api/player/controllers/informations.js
+++ api/player/controllers/informations.js
@@ -1,20 +1,21 @@
 
 async function index(ctx) {
 	const { id } = ctx.params
-	const { getPlayer, getLatestKingdomRank, getAlliance, getHeroes } = strapi.services.player
+	const { getPlayer, getLatestKingdomRank, getLatestTourneyRank, getAlliance, getHeroes } = strapi.services.player
 	const player = await getPlayer(id)
 
 	if (!player) {
 		return ctx.send('Not found', 404)
 	}
 
-	const [rank, alliance, roster] = await Promise.all([
+	const [kingdom, tourney, alliance, roster] = await Promise.all([
 		getLatestKingdomRank(player.id),
+		getLatestTourneyRank(player.id),
 		getAlliance(player.id),
 		getHeroes(player.id),
 	])
 
-	ctx.send({ ...player, rank, alliance, roster })
+	ctx.send({ ...player, alliance, roster, rankings: { kingdom, tourney } })
 }
 
 module.exports = {

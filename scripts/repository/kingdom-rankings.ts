3ad5f529788ec69d4db62d5d4a1e3bc40fe9bdbc
NicolasT <nicolastem@hotmail.fr>
2021-02-26 19:00:20 +0100

    add inactivity

--- scripts/repository/kingdom-rankings.ts
+++ scripts/repository/kingdom-rankings.ts
@@ -1,7 +1,22 @@
 import { differenceInHours, formatISO } from 'date-fns'
+import { reduce } from 'lodash'
 import { Player, KingdomRanking } from '~/types/strapi'
 import { client } from '../services/database'
 import { logger } from '../services/logger'
+
+export const checkInactivity = async (player: Player): Promise<void> => {
+	const rankings: KingdomRanking[] = await client('rankings_kingdom')
+		// @ts-ignore
+		.where('player', '=', player.id)
+		.orderBy('date', 'desc')
+		.limit(4)
+	const sum = reduce(rankings, (sum: number, r: KingdomRanking) => sum + r.power, 0)
+
+	await client('players')
+		.update({ inactive: sum / 4 === rankings[0].power })
+		.where({ id: player.id })
+	logger.success(`Set ${sum / 4 === rankings[0].power ? 'inactive' : 'active'}`)
+}
 
 export const getLatestKingdomRank = async (player: Player): Promise<KingdomRanking> => {
 	const rankings = await client('rankings_kingdom')
@@ -22,6 +37,11 @@ export const createPlayerKingdomRank = async (rank: KingdomRanking): Promise<voi
 		if (differenceInHours(now, old) < 3) {
 			return logger.warn('Player rank already up to date')
 		}
+	}
+
+	if (latest.power === rank.power) {
+		logger.warn('Same power')
+		await checkInactivity(rank.player)
 	}
 
 	await client('rankings_kingdom').insert({

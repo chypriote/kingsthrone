f3c29217a1b5339a85e3d9ec353d51d45a136434
NicolasT <nicolastem@hotmail.fr>
2021-04-18 23:02:13 +0200

    player update roster

--- scripts/player.ts
+++ scripts/player.ts
@@ -1,10 +1,11 @@
 import { config } from 'dotenv'
 config()
-import { client } from './services/requests'
+import { client, LOGIN_ACCOUNT_GAUTIER } from './services/requests'
 import { logger } from './services/logger'
 import { createOrUpdateMyHero } from './repository/roster'
 
 export const updateHeroList = async (): Promise<void> => {
+	await client.login(LOGIN_ACCOUNT_GAUTIER)
 	const game = await client.getGameInfos()
 	const heroList = game.hero.heroList
 

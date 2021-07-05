cc4d1a9aa94fd286da992bef5b679b416cef23fe
Nicolas <nicolas.temenides@assoconnect.com>
2021-07-05 10:03:12 +0200

    upgrade deps

--- scripts/services/requests.ts
+++ scripts/services/requests.ts
@@ -25,7 +25,7 @@ import { CastleInfos } from '~/types/goat/Kingdom'
 import { XSOngoingFight, XSTourneyReward } from '~/types/goat/XSTourney'
 import { DMOngoingFight, DMRanking, DMTourneyReward } from '~/types/goat/DMTourney'
 
-const VERSION = 'V1.3.549'
+const VERSION = 'V1.3.555'
 const COOKIE = 'lyjxncc=c3ac4e77dff349b66c7aeed276e3eb6c'
 export const LOGIN_ACCOUNT_GAUTIER = { 'rsn':'2ylxannmqx','login':{ 'loginAccount':{
 	'parm1':'WIFI','platform':'gaotukc',

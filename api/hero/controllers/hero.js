1b15d96a131057c39b50e04bb00cdf405e458978
NicolasT <nicolastem@hotmail.fr>
2021-07-28 22:26:01 +0200

    update models

--- api/hero/controllers/hero.js
+++ api/hero/controllers/hero.js
@@ -11,7 +11,17 @@ module.exports = {
 		const { id } = ctx.params
 
 		const params = isNaN(id) ? { slug: id.toLowerCase() } : { id }
-		const entity = await strapi.services.hero.findOne(params, ['hero_group.paragons'])
+		const entity = await strapi.services.hero.findOne(params, [
+			'hero_group.paragons',
+			'picture',
+			'skins',
+			'quality_skills',
+			'quality_skills.skins',
+			'quality_skills.skins.picture',
+			'paragons',
+			'maiden',
+			'maiden.picture',
+		])
 
 		return sanitizeEntity(entity, { model: strapi.models.hero })
 	},
